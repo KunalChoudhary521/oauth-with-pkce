916479c5e305485740d8d02ba7fcfb1d93615ea2
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2024-02-24 11:41:34 -0500

    Add H2 database for testing

---
+++ bank-app-api/src/test/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO customer (name, email, mobile_number, pwd, role, create_dt)
+    VALUES ('Happy', 'happy@example.com', '9876548337', '$2y$12$oRRbkNfwuR8ug4MlzH5FOeui.//1mkd.RsOAJMbykTSupVy.x/vb2','admin', CURDATE());
